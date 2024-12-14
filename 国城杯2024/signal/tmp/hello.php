01132cc2f25c77efff804fbf0f50008c9fd7d9f9
Fupanc <149390938+Fupanc@users.noreply.github.com>
2024-12-14 22:30:47 +0800

    Add files via upload

---
+++ 国城杯2024/signal/tmp/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h2>Welcome, guest!</h2>
+    <p>You are successfully logged in.</p>
+    <h3>Go see something</h3>
+</body>
+</html>
